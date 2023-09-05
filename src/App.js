751edcb9c6a83b049354b49169d0bd8a72dd5aa0
eswarmanda <eswar95manda@gmail.com>
2023-09-05 06:15:31 +0000

    Visit Countries react

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import {Component} from 'react'
+import './App.css'
+
+// This is the list (static data) used in the application. You can move it to any component if needed.
+
+const initialCountriesList = [
+  {
+    id: '53c9c67a-c923-4927-8a75-fdfc4bc5ec61',
+    name: 'Australia',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-australia-img.png',
+    isVisited: false,
+  },
+  {
+    id: '8baa8029-fb2c-4f06-bfcc-3dc9ad12b24d',
+    name: 'Canada',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-canada-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1b520f98-6548-41f3-816e-c8b887865172',
+    name: 'Greenland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-greenland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '25841996-fbfd-4554-add4-4c94082c8ccd',
+    name: 'India',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-india-img.png',
+    isVisited: true,
+  },
+  {
+    id: '603c3568-13b0-11ec-82a8-0242ac130003',
+    name: 'Netherlands',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-netherland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '3c988dec-55e1-477d-a9e2-b354fd559849',
+    name: 'Portugal',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-portugal-img.png',
+    isVisited: false,
+  },
+  {
+    id: 'd766f754-34f7-413e-81ec-9992821b97fa',
+    name: 'Switzerland',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-switzerland-img.png',
+    isVisited: false,
+  },
+  {
+    id: '7ebb4e04-b124-417f-a69e-564a456d70f1',
+    name: 'Thailand',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-thailand-img.png',
+    isVisited: false,
+  },
+  {
+    id: '1e4b1dcd-6ace-4dde-ad8d-675927d5ae47',
+    name: 'United Kingdom',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-united-kingdom-img.png',
+    isVisited: true,
+  },
+  {
+    id: 'e76da8ca-bc48-4981-902b-a4d2d46feb6d',
+    name: 'Venezuela',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/visit-countries-venezuela-img.png',
+    isVisited: false,
+  },
+]
+
+// Replace your code here
+class App extends Component {
+  state = {filteredList: initialCountriesList, newList: []}
+
+  componentDidMount() {
+    this.filteredObj()
+  }
+
+  onClickRemove = event => {
+    console.log(event.target.value)
+    const {filteredList} = this.state
+    const updatedList = filteredList.map(eachItem => {
+      if (eachItem.id === event.target.value) {
+        return {...eachItem, isVisited: false}
+      }
+      return eachItem
+    })
+    this.setState({filteredList: updatedList}, this.filteredObj)
+  }
+
+  onClickBtn = event => {
+    console.log(event.target.value)
+    const {filteredList} = this.state
+    const updatedList = filteredList.map(eachItem => {
+      if (eachItem.id === event.target.value) {
+        return {...eachItem, isVisited: true}
+      }
+      return eachItem
+    })
+
+    console.log(updatedList)
+
+    this.setState({filteredList: updatedList}, this.filteredObj)
+  }
+
+  filteredObj = () => {
+    const {filteredList} = this.state
+    const newObj = filteredList.filter(eachItem => eachItem.isVisited === true)
+    console.log(newObj)
+
+    this.setState({newList: newObj})
+  }
+
+  render() {
+    const {filteredList, newList} = this.state
+    return (
+      <div className="main-card">
+        <h1>Countries</h1>
+        <div className="list-card">
+          <ul>
+            {filteredList.map(eachItem => (
+              <li key={eachItem.id} className="list-item">
+                <p>{eachItem.name}</p>
+                {eachItem.isVisited ? (
+                  <p className="visited">Visited</p>
+                ) : (
+                  <button
+                    value={eachItem.id}
+                    type="button"
+                    className={
+                      eachItem.isVisited ? 'Active-btn' : 'inActive-btn'
+                    }
+                    onClick={this.onClickBtn}
+                  >
+                    Visit
+                  </button>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <h1>Visited Countries</h1>
+        <div>
+          {newList.length > 0 ? (
+            <ul className="ul-card1">
+              {newList.map(eachPlace => (
+                <li className="place-card" key={eachPlace.id}>
+                  <img
+                    className="country-img"
+                    src={eachPlace.imageUrl}
+                    alt="thumbnail"
+                  />
+                  <div className="title-card">
+                    <p>{eachPlace.name}</p>
+                    <button
+                      className="remove-btn"
+                      type="button"
+                      onClick={this.onClickRemove}
+                      value={eachPlace.id}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <div className="error-card">
+              <p>No Countries Visited Yet!</p>
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
